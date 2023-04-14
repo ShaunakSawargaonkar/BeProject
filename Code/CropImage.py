c6d04879f12c45cb9d53adc29a09fa4f63ee9d61
Shaunak <shaunaksawargaonkar@gmail.com>
2023-04-15 01:28:26 +0530

    WOrking!!

--- Code/CropImage.py
+++ Code/CropImage.py
@@ -1,5 +1,32 @@
 from PIL import Image  # Import Image class from library.
 
-image = Image.open("example.jpg")  # Load image.
-cropped_image = image.crop((100, 100, 250, 250))  # Crop the image.
-cropped_image.save("example_cropped.jpg")  
+def cropImage(imagePath,ctrInt):
+    ctr=str(ctrInt)
+    folderpath="C:/Users/HP/Desktop/Working EasyOCR_for_boundingbox_nd_Recognition/CroppedImages/"
+    # imagePath="C:\\Users\\HP\\Desktop\\Working EasyOCR_for_boundingbox_nd_Recognition\\Images\\image.jpg"
+    image = Image.open(imagePath)  # Load image.
+
+    width, height = image.size
+    top = height * 2 // 3
+    bottom = height
+    # set the dimensions of the crop box (left, upper, right, lower)
+    cropped_image = image.crop((0, top, width, bottom))  # Crop the image.
+
+    cropped_image.save(folderpath + ctr + ".jpg")  
+
+
+
+# from PIL import Image  # Import Image class from library.
+
+# folderpath="C:/Users/HP/Desktop/Working EasyOCR_for_boundingbox_nd_Recognition/CroppedImages/"
+# imagePath="C:\\Users\\HP\\Desktop\\Working EasyOCR_for_boundingbox_nd_Recognition\\Images\\image.jpg"
+# image = Image.open(imagePath)  # Load image.
+
+# width, height = image.size
+# ctr="1"
+# top = height * 2 // 3
+# bottom = height
+# # set the dimensions of the crop box (left, upper, right, lower)
+# cropped_image = image.crop((0, top, width, bottom))  # Crop the image.
+
+# cropped_image.save(folderpath + ctr + ".jpg")  
--- Code/__init__.py
+++ Code/__init__.py
@@ -1,5 +1,6 @@
 from video2Frame import videoToFrame
 from DabbaNOCR import dabbaNOCR
+from CropImage import cropImage
 import os
 from PIL import Image
 
@@ -7,10 +8,24 @@ from PIL import Image
 VideoLoc='C:/Users/HP/Desktop/Working EasyOCR_for_boundingbox_nd_Recognition/Video/video19.mp4'
 imagePath='Images\image2.jpg'
 
+# 1 Video to frame
 NoOfFrames=videoToFrame(VideoLoc,25)
 
-imageCtr=0
+print("vide to frame done")
+
+# 2 Crop the frame
 folder_path = "ResultFrame"
+
+ctr=0
+for filename in os.listdir(folder_path):
+     if filename.endswith(".jpg"):
+        ctr+=1
+        imagepath = os.path.join(folder_path, filename)
+        cropImage(imagepath,ctr)
+
+# 3 Cropped frame to text
+
+folder_path = "CroppedImages"
 
 FinalString=" "
 
@@ -20,4 +35,32 @@ for filename in os.listdir(folder_path):
         FinalString+=dabbaNOCR(imagepath)
         
 print("YPPPPPPPPPPPP\n" + FinalString)
+
+
+
+
+
+
+
+# # 1 Video to frame
+
+# NoOfFrames=videoToFrame(VideoLoc,25)
+
+# # 2 Crop the frames
+
+# ctr=0
+# for filename in os.listdir(folder_path):
+#      if filename.endswith(".jpg"):
+#         ctr+=1
+#         imagepath = os.path.join(folder_path, filename)
+#         cropImage(imagepath,ctr)
+
+# # FinalString=" "
+
+# # for filename in os.listdir(folder_path):
+# #     if filename.endswith(".jpg"):
+# #         imagepath = os.path.join(folder_path, filename)
+# #         FinalString+=dabbaNOCR(imagepath)
+        
+# # print("YPPPPPPPPPPPP\n" + FinalString)
     
